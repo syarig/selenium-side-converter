018cc0e93104c43fb0a7630e3792f4a85e1d7b2d
syarig <syarig110@yahoo.co.jp>
2019-11-21 18:40:38 +0900

    Move tests of setting and config

---
+++ src/command/__tests__/init.ts
@@ -0,0 +1,80 @@
+import { Config, Setting } from "src/command/init";
+
+describe('Setting', () => {
+  describe('setSettingPath', () => {
+    test('Normal scenario', () => {
+      const inputFile = 'dir1/dir2/inputs/test.side';
+      const settingPath = 'test';
+      const setting = new Setting();
+      setting['inputsDir'] = 'dir1/dir2/inputs/';
+
+      setting.setSettingPath(inputFile);
+      expect(setting['settingPath']).toBe(settingPath);
+    });
+
+    test('Relative path', () => {
+      const inputFile = './dir1/dir2/inputs/test.side';
+      const settingPath = 'test';
+      const setting = new Setting();
+      setting['inputsDir'] = './dir1/dir2/inputs/';
+
+      setting.setSettingPath(inputFile);
+      expect(setting['settingPath']).toBe(settingPath);
+    });
+
+    test('When inputsDir and inputFile path different', () => {
+      const inputFile = 'dir3/dir4/inputs/test.side';
+      const settingPath = 'dir3.dir4.inputs.test';
+      const setting = new Setting();
+      setting['inputsDir'] = 'dir1/dir2/inputs/';
+
+      setting.setSettingPath(inputFile);
+      expect(setting['settingPath']).toBe(settingPath);
+    });
+
+    test('When there is inputFile in current dir', () => {
+      const inputFile = 'test.side';
+      const settingPath = 'test';
+      const setting = new Setting();
+      setting['inputsDir'] = 'dir1/dir2/inputs/';
+
+      setting.setSettingPath(inputFile);
+      expect(setting['settingPath']).toBe(settingPath);
+    });
+  });
+});
+
+describe('Config', () => {
+  describe('getOutputFile', () => {
+    const config = new Config();
+    config.get = jest.fn()
+      .mockImplementation((key: string) => {
+        if (key === 'inputsDir') {
+          return './inputs';
+
+        } else if (key === 'outputsDir') {
+          return './outputs';
+        }
+
+        return '';
+      });
+
+    test('Normal scenario', () => {
+      const inputFile = './inputs/test.side';
+      const outputFile = 'outputs/test.side';
+      expect(config.getOutputFile(inputFile)).toBe(outputFile);
+    });
+
+    test('There is not side file in inputs directory', () => {
+      const inputFile = './hoge/test.side';
+      const outputFile = 'outputs/hoge/test.side';
+      expect(config.getOutputFile(inputFile)).toBe(outputFile);
+    });
+
+    test('Input file is absolute path', () => {
+      const inputFile = '/dir1/dir2/test.side';
+      const outputFile = 'outputs/dir1/dir2/test.side';
+      expect(config.getOutputFile(inputFile)).toBe(outputFile);
+    });
+  });
+});
